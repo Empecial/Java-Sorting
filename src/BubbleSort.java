2265196de3cc4ae3d9ff204462f4f7f08e920439
Empecial <empecial@gmail.com>
2022-12-10 18:57:01 +0100

    bubblesort, GPT quicksort done

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class App {
+public class BubbleSort {
 
     private static List<Integer> unsortedList = new ArrayList<>();
     
